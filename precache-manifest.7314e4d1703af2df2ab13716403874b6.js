6416dabc951dfaa4076a84a63321f74d5b1294ac
jesucluna <jesucluna@gmail.com>
2020-06-11 09:41:35 -0500

    Updates

---
+++ precache-manifest.7314e4d1703af2df2ab13716403874b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e2ef16012206bfdf3e97993deeb70d8",
+    "url": "/MovDiary/index.html"
+  },
+  {
+    "revision": "2c62f71fef570c29834a",
+    "url": "/MovDiary/static/css/2.7f6cef89.chunk.css"
+  },
+  {
+    "revision": "b180ae25d038afd53fe8",
+    "url": "/MovDiary/static/css/main.06af70c1.chunk.css"
+  },
+  {
+    "revision": "2c62f71fef570c29834a",
+    "url": "/MovDiary/static/js/2.b5c3554f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/MovDiary/static/js/2.b5c3554f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b180ae25d038afd53fe8",
+    "url": "/MovDiary/static/js/main.9d3ad091.chunk.js"
+  },
+  {
+    "revision": "c9f93413ffb4e8b8bd3b",
+    "url": "/MovDiary/static/js/runtime-main.2dadb571.js"
+  }
+]);
